345b215b90c0ac1ede52df4e087f5299828e61c5
Marek Tuchowski <marek@tuchowski.com.pl>
2015-08-28 22:56:16 +0200

    Removed surplus tests from is_number_spec.

--- spec/rspec/be_valid_when/is_number_spec.rb
+++ spec/rspec/be_valid_when/is_number_spec.rb
@@ -1,54 +1,5 @@
-require 'active_model'
-
-# @private
-class FakeModel
-  include ActiveModel::Validations
-
-  attr_accessor :number_field
-  attr_accessor :not_number_field
-
-  validates_numericality_of :number_field, greater_than: 40
-  validates_length_of :not_number_field, minimum: 31
-end
-
 describe 'be_valid_when#is_number' do
-  let(:model) { FakeModel.new }
-
-  context 'with no arguments' do
-    let(:description) { /^be valid when #number_field is a number \(42\)$/ }
-
-    let(:passing_matcher) { be_valid_when(:number_field).is_number }
-    let(:failing_matcher) { be_valid_when(:not_number_field).is_number }
-
-    it 'has the correct description' do
-      expect(passing_matcher.description).to match description
-    end
-
-    it 'returns proper result' do
-      expect(passing_matcher.matches? model).to eq true
-      expect(passing_matcher.does_not_match? model).to eq false
-      expect(failing_matcher.matches? model).to eq false
-      expect(failing_matcher.does_not_match? model).to eq true
-    end
-  end
-
   context 'with one argument' do
-    let(:description) { /^be valid when #number_field is a number \(50\)$/ }
-
-    let(:passing_matcher) { be_valid_when(:number_field).is_number 50 }
-    let(:failing_matcher) { be_valid_when(:number_field).is_number 30 }
-
-    it 'has the correct description' do
-      expect(passing_matcher.description).to match description
-    end
-
-    it 'returns proper result' do
-      expect(passing_matcher.matches? model).to eq true
-      expect(passing_matcher.does_not_match? model).to eq false
-      expect(failing_matcher.matches? model).to eq false
-      expect(failing_matcher.does_not_match? model).to eq true
-    end
-
     it 'should fail if passed non number' do
       expect { be_valid_when(:number_field).is_number 'value' }.to raise_error ArgumentError
       expect { be_valid_when(:number_field).is_number '42' }.to raise_error ArgumentError
